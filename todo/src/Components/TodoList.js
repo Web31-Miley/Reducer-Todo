2525866756069d44f9cc80d0bbdcb34f355145eb
Miley Wright <dishaliekwright@yahoo.com>
2020-06-08 12:26:01 -0400

    todolist

--- todo/src/Components/TodoList.js
+++ todo/src/Components/TodoList.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+import {initialState, reducer} from '../Reducers/index';
+import Form from './Form';
 
 const TodoList = () => {
-    
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return(
         <>
-        
+            Hey there
+            <Form state={state} dispatch={dispatch}/>
+            {state.todoList.map( item => {
+                return (
+                    <div onClick={() => dispatch({id: item.id, type: 'REMOVE_ITEM'})} key = {item.id}>
+                        <div>
+                        <span>{item.completed ? '✔': null}</span>
+                        <span>{item.item}</span>
+                        </div>
+                    </div>
+                )
+            })}
+            <button onClick = {() => dispatch({ type: 'CLEAR_COMPLETED'})}>
+                Clear Completed
+            </button>
         </>
     )
 }
